a05a088bc2b6626ea07aadcf3668786057838bab
Khac VU <khacvv0451@gmail.com>
2025-09-16 20:50:11 +0700

    - SPM.

---
+++ Package.swift
@@ -0,0 +1,20 @@
+import PackageDescription
+
+let package = Package(
+    name: "KVLoading",
+    platforms: [
+        .iOS(.v14)
+    ],
+    products: [
+        .library(
+            name: "KVLoading",
+            targets: ["KVLoading"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "KVLoading",
+            path: "KVLoading/Classes"
+        ),
+    ]
+)
